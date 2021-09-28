1da32925135edbc6a5184e45634fa79055d6f660
misantos <mii.santos342@gmail.com>
2021-09-27 23:02:00 -0300

    jogo da velha finalizado

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Jogador:
     nome: str
     num: int
 
+
 # Cria uma lista de listas preencida com o valor
 def crieMatriz(tam, valor):
     matriz = [] # lista vazia
@@ -29,11 +30,12 @@ def printMatriz(matriz):
 
     print("", end=" ")
     print(*matriz[-1], sep = " | ")
+    print("\n")
 
 def checa_horizontal(tabuleiro, linha, coluna)-> int:
     tam_mat = len(tabuleiro)
     count = 0
-    vencedor = 0
+    vencedor = -1
     for i in range (tam_mat):
         for j in range (tam_mat):
             if(tabuleiro[i][j] == tabuleiro[linha][coluna]):
@@ -44,13 +46,14 @@ def checa_horizontal(tabuleiro, linha, coluna)-> int:
                 vencedor = tabuleiro[linha][coluna]
                 return vencedor 
         count = 0
-    return -1
+    
+    return vencedor
 
 
 def checa_vertical(tabuleiro, linha, coluna)-> int:
     tam_mat = len(tabuleiro)
     count = 0
-    vencedor = 0
+    vencedor = -1
     for j in range (tam_mat):
         for i in range (tam_mat):
             if(tabuleiro[i][j] == tabuleiro[linha][coluna]):
@@ -61,13 +64,14 @@ def checa_vertical(tabuleiro, linha, coluna)-> int:
                 vencedor = tabuleiro[linha][coluna]
                 return vencedor
         count = 0
-    return -1
+    
+    return vencedor
 
 
 def checa_diagonal(tabuleiro, linha, coluna)-> int:
     tam_mat = len(tabuleiro)
     count = 0
-    vencedor = 0
+    vencedor = -1
     for d in range (tam_mat):
         for j in range (tam_mat):
             if(j + d >= tam_mat):
@@ -122,55 +126,69 @@ def checa_diagonal(tabuleiro, linha, coluna)-> int:
                 vencedor = tabuleiro[linha][coluna]
                 return vencedor
         count = 0
-    return -1
+    
+    return vencedor
 
 def verificaVencedor(tabuleiro, linha, coluna) -> int:
+    ret = -1
 
-    return checa_horizontal(tabuleiro, linha,coluna) and checa_vertical(tabuleiro, linha,coluna) and checa_diagonal(tabuleiro, linha,coluna)
+    ret = checa_diagonal(tabuleiro, linha,coluna)
+    if ret != -1:
+        return ret
+
+    ret = checa_vertical(tabuleiro, linha,coluna) 
+    if ret != -1:
+        return ret
+
+    ret = checa_horizontal(tabuleiro, linha,coluna)
+    
+    return ret
 
 def main():
+    print("\n      xoxoxoxo JOGO DA VELHA xoxoxox\n\n")
     tam_matriz = int(input("Entre com o tamanho do tabuleiro: "))
     while tam_matriz > 10 or tam_matriz < 3:
-        tam_matriz = int(input("dado invalido, entre novamente com o dado: "))
+        tam_matriz = int(input("\nTamanho inválido. Entre novamente com o tamanho: "))
 
     matriz = crieMatriz(tam_matriz, " ")
-    qnt_jogadores = int(input("Entre com a quantidade de jogadores: "))
+    qnt_jogadores = int(input("\nEntre com a quantidade de jogadores: "))
     while qnt_jogadores > 5 or qnt_jogadores < 2:
-        qnt_jogadores = int(input("dado invalido, entre novamente com o dado: "))
+        qnt_jogadores = int(input("\nQuantidade inválida. Entre novamente com o número de jogadores: "))
+        
 
     vetor_jogadores = []
     for i in range(qnt_jogadores):
-        aux = str(input("Entre com o nome do jogador {}: ".format(i+1)))
+        aux = str(input("\nEntre com o nome do jogador {}: ".format(i+1)))
         temp = Jogador(aux, i+1)
         vetor_jogadores.append(temp)
 
-    nome_arq = str(input("Entre com o caminho para o arquivo a qual deseja salvar os dados: "))
+    nome_arq = str(input("\nEntre com o nome do arquivo a qual deseja salvar os dados: "))
     arquivo = open(nome_arq, 'w')
     arquivo.write('Tamanho do tabuleiro = ' + str(tam_matriz) + '\n')
     arquivo.write('Quantidade de Jogadores = ' + str(qnt_jogadores) + '\n')
     for i in range(len(vetor_jogadores)):
         arquivo.write('Jogador ' + str(i+1) + ' = ' + vetor_jogadores[i].nome + '\n')
-    print('Entre com as jogadas (id, linha, coluna)')
+    print('\nEntre com as jogadas (id, linha, coluna)')
     arquivo.write('Jogadas (id, linha, coluna):' + '\n')
 
     jogadas = 1
     vencedor = -1
     while jogadas <= (len(matriz) ** 2):
-        id_jogador, linha, coluna = input('jogada ' + str(jogadas) + ': ').split()
+        id_jogador, linha, coluna = input('Jogada ' + str(jogadas) + ': ').split()
         linha = int(linha)
         coluna = int(coluna)
         while (linha >= tam_matriz or coluna >= tam_matriz) or matriz[linha][coluna] != " ":
-            print('Jogada errada, tente novamente')
+            print('\nJogada errada. Tente novamente')
             id_jogador, linha, coluna = input('jogada ' + str(jogadas) + ': ').split()
             linha = int(linha)
             coluna = int(coluna)
 
         arquivo.write('Jogada ' + str(jogadas) + ': id=' + str(id_jogador) + ", linha=" + str(linha) + ", coluna=" + str(coluna) + '\n')
         matriz[linha][coluna] = id_jogador
-        vencedor = verificaVencedor(matriz, linha, coluna)
+        vencedor =int(verificaVencedor(matriz, linha, coluna)) 
         if vencedor != -1:
-            vencedor = int(vencedor)
             vencedor -= 1
+            printMatriz(matriz)
             break
         printMatriz(matriz)
         jogadas += 1
